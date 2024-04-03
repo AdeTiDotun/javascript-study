fc831e5db2ba049a242b30bca4ef4f2eb86984d6
Adejumo Daniel <adejumodaniel274@gmail.com>
2024-04-03 16:01:56 +0100

    made a console

--- values_variables/index.js
+++ values_variables/index.js
@@ -1,7 +1,9 @@
 // Variable is a/the container that stores a value in it.
 // There are also conventional ways of writing javascript language.
 // It is also essential to make variables a lot descriptive.
-
+let myFirstJob = "Sales rep";
+let job1 = "sales rep"; //This second example is not descriptive enough.
 // Overall, it's basically a box into which we can store a value.
 
-let firstName = "Daniel"
+let firstName = "Daniel";
+console.log (firstName);
