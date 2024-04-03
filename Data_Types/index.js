6bf5c2c53b771027ef23d7d139a5fa998216c6f5
Adejumo Daniel <adejumodaniel274@gmail.com>
2024-04-03 20:34:34 +0100

    data types

---
+++ Data_Types/index.js
@@ -0,0 +1,16 @@
+// Number data type
+// Used for decimal and intergers
+
+let age = 20;
+console.log(age);
+
+// Strings data type
+// Used for text...,, sequence of characters
+
+let firstName = "Daniel";
+console.log(firstName);
+
+// Boolean data type
+// Used for taking decisions, true or false.
+
+let myAge = true;
