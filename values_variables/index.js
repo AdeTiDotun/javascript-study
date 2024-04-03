411f703b7f16ff00b11b70135783b53073439f67
Adejumo Daniel <adejumodaniel274@gmail.com>
2024-04-03 15:57:32 +0100

    study on values and variables

---
+++ values_variables/index.js
@@ -0,0 +1,7 @@
+// Variable is a/the container that stores a value in it.
+// There are also conventional ways of writing javascript language.
+// It is also essential to make variables a lot descriptive.
+
+// Overall, it's basically a box into which we can store a value.
+
+let firstName = "Daniel"
